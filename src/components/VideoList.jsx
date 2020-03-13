1249c4167a7eba7b5f9f35735e11531e0bd895fc
aminaouini <aminaouini565@gmail.com>
2020-03-13 16:26:19 +0100

    done

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,18 +1,37 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+// src\data\exampleVideoData.js
+
+import VideoListEntry from './videoListEntry.js';
+
+class VideoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ list: this.props.List });
+
+  }
+  render() {
+
+    return (
+      <div className="video-list">
+
+        {
+          this.state.list.map(data => (<VideoListEntry video={data} />))
+        }
+      </div>
+    );
+  }
+}
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
-VideoList.propTypes = {
-  videos: React.PropTypes.array.isRequired
-};
+// VideoList.propTypes = {
+//   videos: React.PropTypes.array.isRequired
+// };
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
